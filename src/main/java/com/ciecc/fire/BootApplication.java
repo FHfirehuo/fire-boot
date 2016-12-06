ad4daa1a048a3597b449c8234f821524af1239a8
fire <gegeaininiaige@outlook.com>
2016-12-06 13:50:54 +0800

    基础配置

--- src/main/java/com/ciecc/fire/BootApplication.java
+++ src/main/java/com/ciecc/fire/BootApplication.java
@@ -1,33 +1,50 @@
 package com.ciecc.fire;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ciecc.fire.config.AuthorSettings;
+
 @RestController
 @SpringBootApplication
+// @ImportResource({"classpath:some-context.xml","classpath:another-context.xml"})
+// //加载xml配置
 public class BootApplication {
-	
+
+	// 只需要定义属性然后注入值，不需要指明文件位置
+	@Value("${book.author}")
+	private String bookAuther;
+
+	@Value("${book.name}")
+	private String bookNmae;
+
+	@Autowired
+	private AuthorSettings authorSettings;
+
 	@RequestMapping("/")
-	String index(){
-		return "Hello Spring Boot";
+	String index() {
+		return "book-name: " + bookNmae + " book-author " + bookAuther + " author-name: " + authorSettings.getName()
+				+ " author-age " + authorSettings.getAge();
 	}
 
 	public static void main(String[] args) {
-		//SpringApplication.run(BootApplication.class, args);  // 1
-		
-//		SpringApplication app = new SpringApplication(BootApplication.class);
-//		app.setBannerMode(Banner.Mode.OFF);
-//		app.run(args); //2
-		
-		new SpringApplicationBuilder(BootApplication.class)
-        .bannerMode(Banner.Mode.OFF)
-        .run(args);
-		
-		
-		
+		// SpringApplication.run(BootApplication.class, args); // 1
+
+		// SpringApplication app = new SpringApplication(BootApplication.class);
+		// //2
+		// app.setBannerMode(Banner.Mode.OFF); //关闭banner
+		// app.run(args);
+
+		new SpringApplicationBuilder(BootApplication.class) // 3
+				.bannerMode(Banner.Mode.OFF) // 关闭banner
+				.run(args);
+
 	}
 }
---
+++ src/main/java/com/ciecc/fire/config/AuthorSettings.java
@@ -0,0 +1,41 @@
+package com.ciecc.fire.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/***
+ * 通过@ConfigurationProperties加载properties文件内的配置，通过prefix属性指定properties的配置前缀，
+ * 通过locations指定properties文件的位置,例如:
+ * 
+ * @ConfigurationProperties(prefix = "author", locations =
+ *                                 {"classpath:config/author.properties"})
+ * 
+ * @author fire
+ *
+ */
+@Component
+@ConfigurationProperties(prefix = "author")
+public class AuthorSettings {
+
+	private String name;
+	private Long age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println("-------------------:" + name);
+		this.name = name;
+	}
+
+	public Long getAge() {
+		return age;
+	}
+
+	public void setAge(Long age) {
+		System.out.println("-------------------:" + age);
+		this.age = age;
+	}
+
+}
